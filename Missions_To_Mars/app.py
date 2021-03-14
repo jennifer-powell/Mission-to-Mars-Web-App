c8cb34b5968f82ab06fd291427e15c0d54bdbe1a
jennifer-powell <jennyruepowell@gmail.com>
2021-03-13 20:04:14 -0600

    half done

---
+++ Missions_To_Mars/app.py
@@ -0,0 +1,15 @@
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def 
+
+@app.route("/scrape")
+def scrape():
+if __name__ == "__main__":
+    app.run(debug=True)
