2e78198eef62bfeae814c2d32a2701d7c36c8519
jennifer-powell <jennyruepowell@gmail.com>
2021-03-16 15:33:32 -0500

    working on html

--- Missions_To_Mars/app.py
+++ Missions_To_Mars/app.py
@@ -1,15 +1,25 @@
 # import necessary libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # create instance of Flask app
 app = Flask(__name__)
-
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
 
 # create route that renders index.html template
 @app.route("/")
-def 
+def index():
+    mars_info = mongo.db.mars.find_one()
+    return render_template("index.html", mars_info=mars_info) 
 
 @app.route("/scrape")
 def scrape():
+    mars_info= mongo.db.mars_info
+    mars_info= scrape_mars.scrape()
+    mars_info.update({}, mars_info, upsert=True)
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- Missions_To_Mars/scrape_mars.py
+++ Missions_To_Mars/scrape_mars.py
@@ -1,18 +1,25 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
+from flask_pymongo import PyMongo
 import pandas as pd
-import time
+import requests
 
-def scrape():
-    executable_path = {'executable_path': 'C:/Users/jenzy/Desktop/Web-Scraping-Challenge/Missions_To_Mars/chromedriver'}
-    browser = Browser('chrome', **executable_path)
+import time
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "C:/Users/jenzy/Desktop/Web-Scraping-Challenge/Missions_To_Mars/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
     
+    
+def scrape():
+    
+    browser = init_browser()
 # Mars News
     url= 'https://mars.nasa.gov/news'
-
+    browser.visit(url)
+    time.sleep(1)
     html = browser.html
     soup = bs(html, 'html.parser')
-
     element = soup.select_one('ul.item_list li.slide')
 
     title = element.find('div', class_="content_title").get_text()
@@ -21,8 +28,9 @@ def scrape():
 
 #JPL Featured Space Image
     jplurl= 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-
+    browser.visit(jplurl)
     button= browser.find_by_css('button.btn.btn-outline-light')
+    button.click()
     html = browser.html
     soup = bs(html, 'html.parser')
     image_url= soup.select_one('div.fancybox-inner img').get('src')
@@ -30,7 +38,8 @@ def scrape():
 
 #Mars Facts
     url = 'https://space-facts.com/mars/'
-    
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
     tables = pd.read_html(url)
    
 
@@ -41,21 +50,42 @@ def scrape():
     soup = bs(html, 'html.parser')
     items = soup.find_all('div', class_='item')
     urls = []
-    title = []
+    name = []
+    for item in items:
+        urls.append(base_url + item.find('a')['href'])
+        name.append(item.find('h3').text.strip())
+    
     html = browser.html
     soup = bs(html, 'html.parser')
     oneurl = base_url+soup.find('img',class_='wide-image')['src']
     img_urls = []
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-    oneurl = base_url+soup.find('img',class_='wide-image')['src']
+    for oneurl in urls:
+        browser.visit(oneurl)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        oneurl = base_url+soup.find('img',class_='wide-image')['src']
+        img_urls.append(oneurl)
+    
     hemisphere_image_urls = []
-    for i in range(len(title)):
-        hemisphere_image_urls.append({'title':title[i],'img_url':img_urls[i]})
+    for i in range(len(name)):
+        hemisphere_image_urls.append({'title':name[i],'img_url':img_urls[i]})
 
 
 
 
+    mars_info = {
+        "title": title,
+        "paragrpah": paragraph,
+        "featured_image_url": featured_image_url,
+        "tables": tables,
+        "name": name,
+        "hemisphere_image_urls": hemisphere_image_urls
+    }
 
+    browser.quit()
 
+    # Return results
+    return mars_info
+
+# if __name__ == '__main__':
+#     scrape()
